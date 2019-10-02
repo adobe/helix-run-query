e20edde51f08ac593f5b44a1d9f06327094e5987
Marquise Rosier <mrosier@adobe.com>
2019-10-01 17:51:14 -0700

    refactor(lint): fix lint issues

--- src/sendquery.js
+++ src/sendquery.js
@@ -19,16 +19,16 @@ function loadQuery(query, dir = null) {
   return fs.readFileSync(path.resolve(dir || __dirname, 'queries', `${query.replace(/^\//, '')}.sql`)).toString();
 }
 
-function getExtraParameters(query){
-  return query.split('\n').
-  filter(e => e.startsWith('---')).
-  filter(e => e.indexOf(':') > 0).
-  map(e => e.substring(4).split(': ')).
-  reduce((acc, val) => {
-    acc[val[0]] = val[1];
-    return acc;
-  }, {})
-
+function getExtraParameters(query) {
+  return query.split('\n')
+    .filter((e) => e.startsWith('---'))
+    .filter((e) => e.indexOf(':') > 0)
+    .map((e) => e.substring(4).split(': '))
+    .reduce((acc, val) => {
+      // eslint-disable-next-line prefer-destructuring
+      acc[val[0]] = val[1];
+      return acc;
+    }, {});
 }
 
 /**
