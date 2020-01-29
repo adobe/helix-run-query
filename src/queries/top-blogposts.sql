64015788a853e3caeb25eadea776789bb55eaea1
Marquise Rosier <mrosier@adobe.com>
2020-01-29 15:06:57 -0800

    feat(top-blogs): make top-blogposts query parametric
    allow top-blogposts query to take in params
    
    fix #116

--- src/queries/top-blogposts.sql
+++ src/queries/top-blogposts.sql
@@ -1,14 +1,17 @@
 --- Authorization: none
 --- Cache-Control: max-age=1296000
+--- contains: https://theblog--davidnuescheler.hlx.page/ms/archive/posts
+--- repo: theblog
+--- owner: davidnuescheler
 --- fromDays: 30
 --- toDays: 0
 --- limit: 10
 SELECT req_url, count(req_http_X_CDN_Request_ID) as reqs 
   FROM `helix_logging_1McGRQOYFuABWBHyD8D4Ux.requests*`
   WHERE 
-    REGEXP_CONTAINS(req_url, r"^https://theblog--davidnuescheler.hlx.page/ms/archive/posts") AND
-    req_http_X_Repo = "theblog" AND
-    req_http_X_Owner = "davidnuescheler" AND
+    REGEXP_CONTAINS(req_url, @contains) AND
+    req_http_X_Repo = @repo AND
+    req_http_X_Owner = @owner AND
     resp_http_Content_Type = "text/html; charset=UTF-8" AND
     status_code = "200" AND
     time_start_usec > CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @fromDays DAY)) AS STRING) AND
@@ -16,4 +19,3 @@ SELECT req_url, count(req_http_X_CDN_Request_ID) as reqs
   GROUP BY req_url
   ORDER BY reqs DESC
 LIMIT @limit
-
