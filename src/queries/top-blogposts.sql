f1e197add5a0bf9d01eada7b44b557682dc67349
Marquise Rosier <mrosier@adobe.com>
2020-01-29 11:16:28 -0800

    fix(query): gets correct posts
    get posts according to new naming style
    
    fix 116

--- src/queries/top-blogposts.sql
+++ src/queries/top-blogposts.sql
@@ -3,15 +3,17 @@
 --- fromDays: 30
 --- toDays: 0
 --- limit: 10
-SELECT REGEXP_EXTRACT(req_url, "/posts/[^.]*") as blog , count(req_http_X_CDN_Request_ID) as reqs 
+SELECT req_url, count(req_http_X_CDN_Request_ID) as reqs 
   FROM `helix_logging_1McGRQOYFuABWBHyD8D4Ux.requests*`
   WHERE 
-    req_url LIKE "%/posts/%" 
-    AND
+    REGEXP_CONTAINS(req_url, r"^https://theblog--davidnuescheler.hlx.page/ms/archive/posts") AND
+    req_http_X_Repo = "theblog" AND
+    req_http_X_Owner = "davidnuescheler" AND
+    resp_http_Content_Type = "text/html; charset=UTF-8" AND
     status_code = "200" AND
     time_start_usec > CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @fromDays DAY)) AS STRING) AND
     time_start_usec < CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @toDays DAY)) AS STRING)
-  GROUP BY
-    blog
+  GROUP BY req_url
   ORDER BY reqs DESC
 LIMIT @limit
+
