c4109a941f22327615f68bcd4f3a44cd1610c143
Marquise Rosier <mrosier@adobe.com>
2020-05-27 14:57:02 -0700

    fix(most-visited.sql): fix sql
    make sql more efficient and follow style guidelines also add parameterizable threshold also add copyright
    
    fix #200

--- src/queries/most-visited.sql
+++ src/queries/most-visited.sql
@@ -1,45 +1,48 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+*/
 --- Authorization: none
 --- Cache-Control: max-age=1296000
 --- fromDays: 30
 --- toDays: 0
+--- threshold: 100
 --- limit: 100
-with topurls as ( SELECT 
-    req_http_X_Owner AS owner, 
-    req_http_X_Repo AS repo,
-    CONCAT(req_http_X_Owner, req_http_X_Repo) as name,
+WITH num_visits AS (
+  SELECT 
+    COUNT(req_url) AS num_reqs,
+    CONCAT(req_http_X_Owner, '--', req_http_X_Repo) AS repo_name,
     req_url,
-    COUNT(time_start_usec) AS requests,
   FROM `helix_logging_1McGRQOYFuABWBHyD8D4Ux.requests*`
   WHERE 
-    resp_http_Content_Type LIKE "%html%" AND
+    resp_http_Content_Type LIKE "text/html%" AND
     status_code = "200" AND
-    req_url NOT LIKE "https://(null)(null)" AND
-    time_start_usec > CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @fromDays DAY)) AS STRING) AND
-    time_start_usec < CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @toDays DAY)) AS STRING)
-  GROUP BY
-    req_url,
-    owner,
-    repo
-  ORDER BY
-    requests DESC
-) (
-  with urls as 
-    (
-      SELECT a.req_url, a.name, a.requests
-      FROM TOPURLS a
-      INNER JOIN (
-          SELECT distinct name, MAX(requests) reqs
-          FROM TOPURLS
-          GROUP BY name
-        ) 
-      b ON a.name = b.name AND a.requests = b.reqs order by a.requests desc
-    )(
-        select req_url 
-        from urls d 
-        where ( 
-          select (
-            select count(name) as count 
-            from urls 
-            where d.name = name group by name)) = 1 
-        order by requests desc limit @limit)
-      )
+    time_start_usec > CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 30 DAY)) AS STRING) AND
+    time_start_usec < CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 0 DAY)) AS STRING)
+  GROUP BY req_url, repo_name
+)
+(
+  WITH max_visits AS (
+    SELECT 
+      MAX(num_reqs) AS max_reqs,
+      repo_name,
+    FROM num_visits
+    GROUP BY repo_name
+  )
+  SELECT m.max_reqs, m.repo_name, n.req_url
+  FROM 
+    max_visits AS m, 
+    num_visits AS n
+  WHERE 
+    m.max_reqs > CAST(@threshold as INT64) AND
+    m.max_reqs = n.num_reqs AND
+    m.repo_name = n.repo_name 
+  ORDER BY m.max_reqs DESC
+)
