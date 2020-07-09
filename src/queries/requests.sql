5b0267fe5b530848bc25faeb0013e4293cf0ef7a
Marquise Rosier <mrosier@adobe.com>
2020-07-09 14:40:33 -0700

    feat(src/queries/requests.sql): add ability to check logs
    enable helix devs to see errs

---
+++ src/queries/requests.sql
@@ -0,0 +1,13 @@
+--- description: log entries of requests occuring in [fromMins, toMins].
+--- Authorization: none
+--- limit: 1000
+--- contains: theblog--adobe.hlx.page
+--- fromMins: 30
+--- toMins: 0
+SELECT *
+FROM `helix_logging_1McGRQOYFuABWBHyD8D4Ux.requests*` 
+WHERE REGEXP_CONTAINS(req_url, @contains) AND
+time_start_usec > CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @fromMins MINUTE)) AS STRING) AND
+time_start_usec < CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @toMins MINUTE)) AS STRING)
+ORDER BY time_start_usec DESC
+LIMIT @limit
