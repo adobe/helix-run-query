7a988c949f8c704ea19d68cc05225d728cb89a24
Lars Trieloff <trieloff@adobe.com>
2022-01-05 15:58:05 +0100

    feat(rum): wip: rum sources

---
+++ src/queries/rum-sources.sql
@@ -0,0 +1,44 @@
+--- description: Get popularity data for RUM source attribute values, filtered by checkpoint
+--- Authorization: none
+--- Access-Control-Allow-Origin: *
+--- limit: 30
+--- offset: 0
+--- url: 
+--- checkpoint: -
+
+WITH 
+current_data AS (
+  SELECT 
+    TIMESTAMP_TRUNC(TIMESTAMP_MILLIS(CAST(time AS INT64)), DAY) AS date,
+     * 
+  FROM `helix-225321.helix_rum.rum*`
+  WHERE 
+    # use date partitioning to reduce query size
+    _TABLE_SUFFIX <= CONCAT(CAST(EXTRACT(YEAR FROM CURRENT_TIMESTAMP()) AS String), LPAD(CAST(EXTRACT(MONTH FROM CURRENT_TIMESTAMP()) AS String), 2, "0")) AND
+    _TABLE_SUFFIX >= CONCAT(CAST(EXTRACT(YEAR FROM TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL CAST(@limit AS INT64) DAY)) AS String), LPAD(CAST(EXTRACT(MONTH FROM TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL CAST(@limit AS INT64) DAY)) AS String), 2, "0")) AND
+    CAST(time AS STRING) > CAST(UNIX_MICROS(TIMESTAMP_SUB(TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), DAY), INTERVAL SAFE_ADD(CAST(@limit AS INT64), -1) DAY)) AS STRING) AND
+    CAST(time AS STRING) < CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL SAFE_ADD(CAST(@offset AS INT64), 0) DAY)) AS STRING) AND
+    url LIKE CONCAT("https://", @url, "%")
+),
+sources AS (
+ SELECT 
+    id,
+    source, 
+    checkpoint,
+    MAX(url) AS url, 
+    MAX(weight) AS views
+  FROM current_data 
+  WHERE source IS NOT NULL AND (@checkpoint = '-' OR @checkpoint = checkpoint)
+  GROUP BY source, id, checkpoint 
+)
+
+SELECT 
+  COUNT(id) AS ids,
+  COUNT(DISTINCT url) AS pages,
+  SUM(views) AS views,
+  checkpoint,
+  source,
+FROM sources
+GROUP BY source, checkpoint
+ORDER BY views DESC
+LIMIT @limit
