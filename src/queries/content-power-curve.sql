2f0b80f2283d9dee441f9ba9915f03c02cdbad9b
Lars Trieloff <lars@trieloff.net>
2022-05-20 19:11:47 +0200

    refactor(power-curve): use clustered tables and table valued functions

--- src/queries/content-power-curve.sql
+++ src/queries/content-power-curve.sql
@@ -5,37 +5,22 @@
 --- interval: 60
 --- offset: 0
 --- domain: -
-DECLARE upperdate STRING DEFAULT CONCAT(
-  CAST(EXTRACT(YEAR FROM TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL CAST(@offset AS INT64) DAY)) AS String), 
-  LPAD(CAST(EXTRACT(MONTH FROM TIMESTAMP_SUB(
-    CURRENT_TIMESTAMP(), 
-    INTERVAL CAST(@offset AS INT64) DAY)) AS String), 2, "0"));
-
-DECLARE lowerdate STRING DEFAULT CONCAT(
-  CAST(EXTRACT(YEAR FROM TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL SAFE_ADD(CAST(@interval AS INT64), CAST(@offset AS INT64)) DAY)) AS String), 
-  LPAD(CAST(EXTRACT(MONTH FROM TIMESTAMP_SUB(
-    CURRENT_TIMESTAMP(), 
-    INTERVAL SAFE_ADD(CAST(@interval AS INT64), CAST(@offset AS INT64)) DAY)) AS String), 2, "0"));
-
-DECLARE uppertimestamp STRING DEFAULT CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL CAST(@offset AS INT64) DAY)) AS STRING);
-
-DECLARE lowertimestamp STRING DEFAULT CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL SAFE_ADD(CAST(@interval AS INT64), CAST(@offset AS INT64)) DAY)) AS STRING);
-
 WITH visits AS (
   SELECT 
-    MAX(url) AS url, 
-    TIMESTAMP_TRUNC(TIMESTAMP_MILLIS(CAST(MAX(time) AS INT64)), DAY) AS time, 
+    url, 
+    TIMESTAMP_TRUNC(time, DAY) AS time, 
     id, 
-    MAX(weight) AS weight 
-  FROM `helix-225321.helix_rum.rum*` 
-  WHERE 
-    # use date partitioning to reduce query size
-    _TABLE_SUFFIX <= upperdate AND
-    _TABLE_SUFFIX >= lowerdate AND
-    CAST(time AS STRING) < uppertimestamp AND
-    CAST(time AS STRING) > lowertimestamp AND
-    (@domain = "-" OR url LIKE CONCAT("https://", @domain, "%"))
-  GROUP BY id
+    pageviews AS weight
+  FROM helix_rum.CLUSTER_PAGEVIEWS(
+    @domain,
+    CAST(@offset AS INT64),
+    CAST(@interval AS INT64) + 1,
+    '2022-02-01', # not used, start date
+    '2022-05-28', # not used, end date
+    'GMT', # timezone
+    'all', # device class
+    '-' # not used, generation
+  )
 ),
 urldays AS (
   SELECT time, url, COUNT(id) AS events, SUM(weight) AS visits FROM visits # FULL JOIN days ON (days.time = visits.time)
