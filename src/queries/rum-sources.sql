2f850de153978e8346767c544ae943e7fcb4eef0
Lars Trieloff <lars@trieloff.net>
2022-05-23 11:38:56 +0200

    refactor(rum-sources): use clustered tables as data input

--- src/queries/rum-sources.sql
+++ src/queries/rum-sources.sql
@@ -4,22 +4,24 @@
 --- limit: 30
 --- interval: 30
 --- offset: 0
---- url: 
+--- url: -
 --- checkpoint: -
 
 WITH 
 current_data AS (
   SELECT 
-    TIMESTAMP_TRUNC(TIMESTAMP_MILLIS(CAST(time AS INT64)), DAY) AS date,
+    TIMESTAMP_TRUNC(time, DAY) AS date,
      * 
-  FROM `helix-225321.helix_rum.rum*`
-  WHERE 
-    # use date partitioning to reduce query size
-    _TABLE_SUFFIX <= CONCAT(CAST(EXTRACT(YEAR FROM CURRENT_TIMESTAMP()) AS String), LPAD(CAST(EXTRACT(MONTH FROM CURRENT_TIMESTAMP()) AS String), 2, "0")) AND
-    _TABLE_SUFFIX >= CONCAT(CAST(EXTRACT(YEAR FROM TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL CAST(@interval AS INT64) DAY)) AS String), LPAD(CAST(EXTRACT(MONTH FROM TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL CAST(@interval AS INT64) DAY)) AS String), 2, "0")) AND
-    CAST(time AS STRING) > CAST(UNIX_MICROS(TIMESTAMP_SUB(TIMESTAMP_TRUNC(CURRENT_TIMESTAMP(), DAY), INTERVAL SAFE_ADD(CAST(@interval AS INT64), -1) DAY)) AS STRING) AND
-    CAST(time AS STRING) < CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL SAFE_ADD(CAST(@offset AS INT64), 0) DAY)) AS STRING) AND
-    url LIKE CONCAT("https://", @url, "%")
+  FROM helix_rum.CLUSTER_CHECKPOINTS(
+    @url,
+    CAST(@offset AS INT64),
+    CAST(@interval AS INT64),
+    '2022-01-01',
+    '2022-01-31',
+    'UTC',
+    'all',
+    '-'
+  )
 ),
 sources AS (
  SELECT 
@@ -27,8 +29,8 @@ sources AS (
     source, 
     checkpoint,
     MAX(url) AS url, 
-    MAX(weight) AS views,
-    SUM(weight) AS actions,
+    MAX(pageviews) AS views,
+    SUM(pageviews) AS actions,
   FROM current_data 
   WHERE source IS NOT NULL AND (@checkpoint = '-' OR @checkpoint = checkpoint)
   GROUP BY source, id, checkpoint 
