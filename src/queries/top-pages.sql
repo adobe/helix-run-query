4e740c30c1645204c9460fbd409c7780764769ab
Lars Trieloff <trieloff@adobe.com>
2022-05-24 11:44:00 +0200

    build(sqlfluff): adjust linting rules

--- src/queries/top-pages.sql
+++ src/queries/top-pages.sql
@@ -5,44 +5,44 @@
 --- toDays: 0
 --- limit: 10
 SELECT * FROM (
-        SELECT
-            req_url,
-            count(req_http_x_cdn_request_id) AS reqs
-        FROM (
-            ^allrequests(
-                    resp_http_content_type,
-                    status_code,
-                    time_start_usec,
-                    req_url,
-                    req_http_x_cdn_request_id
-                )
-            WHERE
-                _table_suffix BETWEEN
-                format_timestamp(
-                    "%Y%m",
-                    timestamp_sub(current_timestamp(), INTERVAL @fromDays DAY)
-                ) AND
-                format_timestamp(
-                    "%Y%m",
-                    timestamp_sub(current_timestamp(), INTERVAL @toDays DAY)
-                )
-        )
-        WHERE
-            resp_http_content_type LIKE "text/html%"
-            AND status_code = "200" AND
-            time_start_usec > cast(
-                unix_micros(
-                    timestamp_sub(current_timestamp(), INTERVAL @fromDays DAY)
-                ) AS STRING
-            ) AND
-            time_start_usec < cast(
-                unix_micros(
-                    timestamp_sub(current_timestamp(), INTERVAL @toDays DAY)
-                ) AS STRING
-            )
-        GROUP BY
-            req_url
-        ORDER BY reqs DESC
+  SELECT
+    req_url,
+    COUNT(req_http_x_cdn_request_id) AS reqs
+  FROM (
+    ^allrequests(
+        resp_http_content_type,
+        status_code,
+        time_start_usec,
+        req_url,
+        req_http_x_cdn_request_id
+      )
+    WHERE
+      _table_suffix BETWEEN
+      FORMAT_TIMESTAMP(
+        "%Y%m",
+        TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @fromDays DAY)
+      ) AND
+      format_timestamp(
+        "%Y%m",
+        TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @toDays DAY)
+      )
+  )
+  WHERE
+    resp_http_content_type LIKE "text/html%"
+    AND status_code = "200" AND
+    time_start_usec > CAST(
+      UNIX_MICROS(
+        TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @fromDays DAY)
+      ) AS STRING
+    ) AND
+    time_start_usec < CAST(
+      UNIX_MICROS(
+        TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL @toDays DAY)
+      ) AS STRING
+    )
+  GROUP BY
+    req_url
+  ORDER BY reqs DESC
 )
 WHERE reqs > 10
 LIMIT @limit
