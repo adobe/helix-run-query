0fe76e0504154c2ccf4842cbb8c6c857fccf3fb5
Lars Trieloff <lars@trieloff.net>
2022-10-27 14:52:57 -0600

    style(rum-targets): sqlfluff autoformat

--- src/queries/rum-targets.sql
+++ src/queries/rum-targets.sql
@@ -10,11 +10,11 @@
 --- separator: ;
 --- extract: -
 
-WITH 
+WITH
 current_data AS (
-    SELECT 
-    TIMESTAMP_TRUNC(time, DAY) AS date,
-     * 
+  SELECT
+    *,
+    TIMESTAMP_TRUNC(time, DAY) AS date
   FROM helix_rum.CLUSTER_CHECKPOINTS(
     @url,
     CAST(@offset AS INT64),
@@ -26,28 +26,41 @@ current_data AS (
     '-'
   )
 ),
+
 targets AS (
- SELECT 
+  SELECT
     id,
-    target, 
+    target,
     checkpoint,
-    REGEXP_REPLACE(MAX(url), r"\?.*$", "") AS url, 
+    REGEXP_REPLACE(MAX(url), r'\?.*$', '') AS url,
     MAX(pageviews) AS views
-  FROM current_data 
-  WHERE target IS NOT NULL 
-    AND (CAST(@checkpoint AS STRING) = '-' OR (CAST(@checkpoint AS STRING) = checkpoint))
+  FROM current_data
+  WHERE target IS NOT NULL
+    AND (
+      CAST(
+        @checkpoint AS STRING
+      ) = '-' OR CAST(@checkpoint AS STRING) = checkpoint
+    )
     AND (@source = '-' OR @source = source)
-  GROUP BY target, id, checkpoint 
+  GROUP BY target, id, checkpoint
 )
 
-SELECT 
+SELECT
+  checkpoint,
+  target,
   COUNT(id) AS ids,
   COUNT(DISTINCT url) AS pages,
   APPROX_TOP_COUNT(url, 1)[OFFSET(0)].value AS topurl,
-  SUM(views) AS views,
-  checkpoint,
-  target,
-FROM targets, UNNEST(IF(@extract = '-', SPLIT(target, CONCAT(@separator, " ")), REGEXP_EXTRACT_ALL(target, @extract))) AS target
+  SUM(views) AS views
+FROM
+  targets,
+  UNNEST(
+    IF(
+      @extract = '-',
+      SPLIT(target, CONCAT(@separator, ' ')),
+      REGEXP_EXTRACT_ALL(target, @extract)
+    )
+  ) AS target
 GROUP BY target, checkpoint
 ORDER BY views DESC
 LIMIT @limit
