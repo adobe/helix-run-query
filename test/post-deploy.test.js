f494d40cc135798afb6d5a74125908dad0e29b2f
Lars Trieloff <lars@trieloff.net>
2023-03-25 17:35:39 -0700

    test(post-deploy): use query that still exists in post-deploy testing

--- test/post-deploy.test.js
+++ test/post-deploy.test.js
@@ -26,7 +26,7 @@ createTargets().forEach((target) => {
     });
 
     it('Most Visited', async () => {
-      const path = `${target.urlPath()}/most-visited-hlx3`;
+      const path = `${target.urlPath()}/rum-dashboard`;
       // eslint-disable-next-line no-console
       console.log(`testing ${target.host()}${path}`);
       await chai
