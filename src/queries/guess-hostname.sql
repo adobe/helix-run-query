aea9366003cd7f4828c29323ee6df93d88551e35
Lars Trieloff <lars@trieloff.net>
2023-03-25 18:25:51 -0700

    style(guess-hostname): fix sqlfluff complaints

--- src/queries/guess-hostname.sql
+++ src/queries/guess-hostname.sql
@@ -7,32 +7,68 @@
 --- repo: -
 --- ref: -
 WITH data AS (
-SELECT 
+  SELECT
+    req_http_x_owner AS owner,
+    req_http_x_repo AS repo,
+    req_http_x_ref AS ref,
     COUNT(time_start_usec) AS requests,
-    REGEXP_EXTRACT(req_http_Referer, r"https://([^/]+)", 1) AS host, 
-    req_http_X_Owner AS owner, 
-    req_http_X_Repo AS repo,
-    req_http_X_Ref AS ref,
-FROM `helix-225321.helix_logging_7TvULgs0Xnls4q3R8tawdg.requests*` 
-WHERE 
+    REGEXP_EXTRACT(req_http_referer, r"https://([^/]+)", 1) AS host
+  FROM `helix-225321.helix_logging_7TvULgs0Xnls4q3R8tawdg.requests*`
+  WHERE
     # use date partitioning to reduce query size
-    _TABLE_SUFFIX <= CONCAT(CAST(EXTRACT(YEAR FROM CURRENT_TIMESTAMP()) AS String), LPAD(CAST(EXTRACT(MONTH FROM CURRENT_TIMESTAMP()) AS String), 2, "0")) AND
-    _TABLE_SUFFIX >= CONCAT(CAST(EXTRACT(YEAR FROM TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL CAST(@interval AS INT64) DAY)) AS String), LPAD(CAST(EXTRACT(MONTH FROM TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 30 DAY)) AS String), 2, "0")) AND
-    CAST(time_start_usec AS STRING) > CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL CAST(@interval AS INT64) DAY)) AS STRING) AND
-    CAST(time_start_usec AS STRING) < CAST(UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 0 DAY)) AS STRING) AND
-    REGEXP_CONTAINS(req_http_Referer, r"https://[^/]+/.*")
-    AND REGEXP_EXTRACT(req_http_Referer, r"https://([^/]+)", 1) NOT LIKE "%.hlx3.page"
-    AND REGEXP_EXTRACT(req_http_Referer, r"https://([^/]+)", 1) NOT LIKE "%.hlx.live"
-GROUP BY 
-    req_http_X_Repo, 
-    req_http_X_Owner, 
-    req_http_X_Ref,
+    _table_suffix
+    <= CONCAT(
+      CAST(EXTRACT(YEAR FROM CURRENT_TIMESTAMP()) AS String),
+      LPAD(CAST(EXTRACT(MONTH FROM CURRENT_TIMESTAMP()) AS String), 2, "0")
+    )
+    AND _table_suffix >= CONCAT(
+      CAST(
+        EXTRACT(
+          YEAR FROM TIMESTAMP_SUB(
+            CURRENT_TIMESTAMP(), INTERVAL CAST(@interval AS INT64) DAY
+          )
+        ) AS String
+      ),
+      LPAD(
+        CAST(
+          EXTRACT(
+            MONTH FROM TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 30 DAY)
+          ) AS String
+        ),
+        2,
+        "0"
+      )
+    )
+    AND CAST(time_start_usec AS STRING)
+    > CAST(
+      UNIX_MICROS(
+        TIMESTAMP_SUB(
+          CURRENT_TIMESTAMP(), INTERVAL CAST(@interval AS INT64) DAY
+        )
+      ) AS STRING
+    )
+    AND CAST(time_start_usec AS STRING)
+    < CAST(
+      UNIX_MICROS(TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL 0 DAY)) AS STRING
+    )
+    AND REGEXP_CONTAINS(req_http_referer, r"https://[^/]+/.*")
+    AND REGEXP_EXTRACT(
+      req_http_referer, r"https://([^/]+)", 1
+    ) NOT LIKE "%.hlx3.page"
+    AND REGEXP_EXTRACT(
+      req_http_referer, r"https://([^/]+)", 1
+    ) NOT LIKE "%.hlx.live"
+  GROUP BY
+    req_http_x_repo,
+    req_http_x_owner,
+    req_http_x_ref,
     host
-ORDER BY requests DESC
+  ORDER BY requests DESC
 )
+
 SELECT * FROM data WHERE
-    (owner = @owner OR @owner = "-") AND
-    (repo = @repo OR @repo = "-") AND
-    (ref = @ref OR @ref = "-") AND
-    (host = @url OR @url = "-")
+  (owner = @owner OR @owner = "-")
+  AND (repo = @repo OR @repo = "-")
+  AND (ref = @ref OR @ref = "-")
+  AND (host = @url OR @url = "-")
 LIMIT CAST(@limit AS INT64)
