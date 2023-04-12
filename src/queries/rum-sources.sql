a68bbe1c094e3c97b0ef46427548f8ce58e248ac
Lars Trieloff <lars@trieloff.net>
2023-04-12 09:55:14 +0200

    style(rum-sources): fix indentation

--- src/queries/rum-sources.sql
+++ src/queries/rum-sources.sql
@@ -10,46 +10,46 @@
 
 WITH
 current_data AS (
-  SELECT *
-  FROM helix_rum.CLUSTER_CHECKPOINTS(
-    @url,
-    CAST(@offset AS INT64),
-    CAST(@interval AS INT64),
-    '2022-01-01',
-    '2022-01-31',
-    'UTC',
-    'all',
-    '-'
-  )
+    SELECT *
+    FROM helix_rum.CLUSTER_CHECKPOINTS(
+        @url,
+        CAST(@offset AS INT64),
+        CAST(@interval AS INT64),
+        '2022-01-01',
+        '2022-01-31',
+        'UTC',
+        'all',
+        '-'
+        )
 ),
 
 sources AS (
-  SELECT
-    id,
-    source,
-    checkpoint,
-    MAX(url) AS url,
-    MAX(pageviews) AS views,
-    SUM(pageviews) AS actions
-  FROM current_data
-  WHERE
-    source IS NOT NULL AND (
-      CAST(
-        @checkpoint AS STRING
-      ) = '-' OR CAST(@checkpoint AS STRING) = checkpoint
-    ) AND (source = @source OR @source = '-')
-  GROUP BY source, id, checkpoint
+    SELECT
+        id,
+        source,
+        checkpoint,
+        MAX(url) AS url,
+        MAX(pageviews) AS views,
+        SUM(pageviews) AS actions
+    FROM current_data
+    WHERE
+        source IS NOT NULL AND (
+            CAST(
+                @checkpoint AS STRING
+            ) = '-' OR CAST(@checkpoint AS STRING) = checkpoint
+        ) AND (source = @source OR @source = '-')
+    GROUP BY source, id, checkpoint
 )
 
 SELECT
-  checkpoint,
-  source,
-  COUNT(id) AS ids,
-  COUNT(DISTINCT url) AS pages,
-  APPROX_TOP_COUNT(url, 1)[OFFSET(0)].value AS topurl,
-  SUM(views) AS views,
-  SUM(actions) AS actions,
-  SUM(actions) / SUM(views) AS actions_per_view
+    checkpoint,
+    source,
+    COUNT(id) AS ids,
+    COUNT(DISTINCT url) AS pages,
+    APPROX_TOP_COUNT(url, 1)[OFFSET(0)].value AS topurl,
+    SUM(views) AS views,
+    SUM(actions) AS actions,
+    SUM(actions) / SUM(views) AS actions_per_view
 FROM sources
 GROUP BY source, checkpoint
 ORDER BY views DESC
