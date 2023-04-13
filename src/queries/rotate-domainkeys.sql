2f2888f9c7e52835c1dcaba552f74bb0ad93bfdb
Lars Trieloff <lars@trieloff.net>
2023-04-13 11:00:13 +0200

    style(queries): fix indentation of rotator query

--- src/queries/rotate-domainkeys.sql
+++ src/queries/rotate-domainkeys.sql
@@ -9,30 +9,32 @@
 DECLARE newkey STRING;
 
 IF EXISTS(
-    SELECT * FROM `helix-225321.helix_reporting.domain_keys`
-    WHERE key = @domainkey AND
-      (revoke_date IS NULL
-      OR revoke_date > CURRENT_DATE(@timezone)) and
-    (hostname_prefix = "" OR hostname_prefix = @url)
-  ) THEN
-  SET newkey = IF(@newkey = "-", GENERATE_UUID(), @newkey);
-  CALL helix_rum.ROTATE_DOMAIN_KEYS(
-  @domainkey,
-  IF(@url = "-", "", @url),
-  @timezone,
-  CAST(@graceperiod AS INT64),
-  @expiry,
-  newkey);
+    SELECT
+        hostname_prefix,
+        key,
+        revoke_date
+    FROM `helix-225321.helix_reporting.domain_keys`
+    WHERE key = @domainkey
+        AND (revoke_date IS NULL OR revoke_date > CURRENT_DATE(@timezone))
+        AND (hostname_prefix = "" OR hostname_prefix = @url)
+) THEN
+    SET newkey = IF(@newkey = "-", GENERATE_UUID(), @newkey);
+    CALL helix_rum.ROTATE_DOMAIN_KEYS(
+        @domainkey,
+        IF(@url = "-", "", @url),
+        @timezone,
+        CAST(@graceperiod AS INT64),
+        @expiry,
+        newkey);
 END IF;
 
-SELECT 
-  IF(@url = "-", "", @url) AS hostname_prefix,
-  IF(EXISTS(
-    SELECT * FROM `helix-225321.helix_reporting.domain_keys`
-    WHERE key = @domainkey AND
-      (revoke_date IS NULL
-      OR revoke_date > CURRENT_DATE(@timezone)) and
-    (hostname_prefix = "" OR hostname_prefix = @url)
-  ), "success", "failure") AS status,
-  newkey AS key,
-  IF(@expiry = "-", NULL, @expiry) AS revoke_date
+SELECT
+    newkey AS key,
+    IF(@url = "-", "", @url) AS hostname_prefix,
+    IF(EXISTS(
+        SELECT * FROM `helix-225321.helix_reporting.domain_keys`
+        WHERE key = @domainkey
+            AND (revoke_date IS NULL OR revoke_date > CURRENT_DATE(@timezone))
+            AND (hostname_prefix = "" OR hostname_prefix = @url)
+        ), "success", "failure") AS status,
+    IF(@expiry = "-", NULL, @expiry) AS revoke_date
