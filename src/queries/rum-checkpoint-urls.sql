d6263fb5d485a6688975fb1438017a7407c0d6c6
Lars Trieloff <lars@trieloff.net>
2023-04-20 10:05:49 +0200

    fix(rum-checkpoint-urls): avoid type error
    see https://cq-dev.slack.com/archives/CGG9B79BK/p1681977737441489

--- src/queries/rum-checkpoint-urls.sql
+++ src/queries/rum-checkpoint-urls.sql
@@ -8,42 +8,50 @@
 --- checkpoint: -
 --- source: -
 
-WITH 
+WITH
 current_data AS (
-    SELECT 
-    TIMESTAMP_TRUNC(time, DAY) AS date,
-     * 
-  FROM helix_rum.CLUSTER_CHECKPOINTS(
-    @url,
-    CAST(@offset AS INT64),
-    CAST(@interval AS INT64),
-    '2022-01-01',
-    '2022-01-31',
-    'UTC',
-    'all',
-    '-'
-  )
+  SELECT
+    *,
+    TIMESTAMP_TRUNC(time, DAY) AS date
+  FROM
+    helix_rum.CLUSTER_CHECKPOINTS(
+      @url,
+      CAST(@offset AS INT64),
+      CAST(@interval AS INT64),
+      '2022-01-01',
+      '2022-01-31',
+      'UTC',
+      'all',
+      '-'
+    )
 ),
+
 checkpoint_urls AS (
-  SELECT COUNT(id) AS ids, 
+  SELECT
+    url,
+    checkpoint,
+    source,
+    COUNT(id) AS ids,
     MAX(pageviews) AS views,
-    SUM(pageviews) AS actions,
-    url, 
-    checkpoint, 
-    source 
+    SUM(pageviews) AS actions
   FROM current_data
-  WHERE (checkpoint = @checkpoint OR @checkpoint = '-') AND
-        (source = @source OR @source = '-')
+  WHERE
+    (
+      checkpoint = CAST(@checkpoint AS STRING)
+      OR CAST(@checkpoint AS STRING) = '-'
+    )
+    AND (source = @source OR @source = '-')
   GROUP BY url, checkpoint, source
 )
-SELECT 
+
+SELECT
   ids,
   views,
   actions,
-  actions / views AS actions_per_view,
   url,
   checkpoint,
-  source
+  source,
+  actions / views AS actions_per_view
 FROM checkpoint_urls
 ORDER BY views DESC
 LIMIT CAST(@limit AS INT64)
