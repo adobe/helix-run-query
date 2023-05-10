3c883d33e0ab963b4840af3e88f0f1ffe2391de0
langswei <33439846+langswei@users.noreply.github.com>
2023-05-10 10:57:55 -0500

    Create rum-pageviews-bdf.sql
    for BDF POC to combine CS and Franklin page views

---
+++ src/queries/rum-pageviews-bdf.sql
@@ -0,0 +1,39 @@
+--- description: Show page views for specified domains for specified dates based on extrapolation from RUM data
+--- Authorization: none
+--- interval: 30
+--- offset: 0
+--- startdate: 2023-05-01
+--- enddate: 2023-06-01
+--- timezone: UTC
+--- url: -
+--- device: all
+--- domainkey: secret
+
+WITH rum AS (
+  SELECT
+    REGEXP_REPLACE(hostname, 'www.', '') AS hostname,
+    COUNT(DISTINCT id) AS rum_count,
+    weight,
+    FORMAT_DATE('%Y-%m-%d', time) AS date
+  FROM helix_rum.EVENTS_V3(
+    @url,
+    CAST(@offset AS INT64),
+    CAST(@interval AS INT64),
+    @startdate,
+    @enddate,
+    @timezone,
+    @device,
+    @domainkey
+  )
+  GROUP BY date, weight, hostname
+)
+SELECT
+  a.hostname,
+  SUM(a.rum_count * a.weight) AS estimated_pv,
+  a.date,
+  b.ims_org_id
+FROM rum a
+INNER JOIN `helix_reporting.domain_info` b ON a.hostname = b.domain
+AND b.ims_org_id != ''
+GROUP BY a.hostname, a.date, b.ims_org_id
+ORDER BY a.hostname, a.date
