c4ab998a166724ca8f93a0497b68aa59691f7c37
Marquise Rosier <mrosier@adobe.com>
2023-09-15 15:49:04 -0500

    make legend

---
+++ src/queries/rum-dashboard/sidekick-checkpoints.sql
@@ -0,0 +1,27 @@
+--- description: Get All Sidekick Checkpoints From RUM for a given domain
+--- Authorization: none
+--- Access-Control-Allow-Origin: *
+--- limit: 30
+--- interval: 30
+--- offset: 0
+--- startdate: 2023-02-01
+--- enddate: 2023-05-28
+--- timezone: UTC
+--- url: -
+--- device: all
+--- domainkey: secret
+with sidekick_events AS (
+SELECT
+  FORMAT_DATE("%Y-%m-%d", DATE_TRUNC(time, DAY)) AS day,
+  id,
+  checkpoint,
+  hostname,
+  source,
+  user_agent LIKE "%Sidekick%" AS extension
+FROM   helix_rum.CHECKPOINTS_V4(@url, @offset, @interval, @startdate, @enddate, @timezone, 'all', @domainkey )
+WHERE  CHECKPOINT LIKE "sidekick:%")
+SELECT 
+     CHECKPOINT,
+FROM     sidekick_events
+GROUP BY checkpoint
+ORDER BY checkpoint desc
