3cfe7b3f8915026e854c497d7674f158df738afe
Damian Langsweirdt <langswei@adobe.com>
2023-09-28 15:56:26 -0500

    version test

---
+++ src/queries/dash/version.sql
@@ -0,0 +1,14 @@
+--- description: List of domains along with some summary data.
+--- Access-Control-Allow-Origin: *
+--- Cache-Control: max-age=3600
+--- timezone: UTC
+--- device: all
+--- domainkey: secret
+--- interval: 365
+--- offset: 0
+--- startdate: 2022-02-01
+--- enddate: 2022-05-28
+--- url: -
+--- version: -
+
+SELECT @version
