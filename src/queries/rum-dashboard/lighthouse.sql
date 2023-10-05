843df5f7afd885fe33610b83f88a95612d3c694d
Marquise Rosier <mrosier@adobe.com>
2023-10-04 18:55:53 -0700

    add support for lighthouse scores

---
+++ src/queries/rum-dashboard/lighthouse.sql
@@ -0,0 +1,28 @@
+--- description: Get Daily Lighthouse Scores For a Site
+--- Authorization: none
+--- Access-Control-Allow-Origin: *
+--- limit: 30
+--- interval: 30
+--- offset: 0
+--- startdate: 2023-02-01
+--- enddate: 2023-05-28
+--- timezone: UTC
+--- timeunit: day
+--- url: -
+--- device: all
+--- domainkey: secret
+with current_data as (
+SELECT 
+  * 
+FROM 
+  helix_external_data.LIGHTHOUSE_SCORES(
+    @url, 
+    @offset, 
+    @interval, 
+    @startdate,
+    @enddate, 
+    @domainkey
+  )
+) 
+select * from current_data
+order by url, time asc
