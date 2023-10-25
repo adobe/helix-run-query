1c29e3d3dc9c209d1a8b041456fec90f7b8c04ff
Marquise Rosier <mrosier@adobe.com>
2023-10-25 00:52:00 -0700

    checkpoint search

--- src/queries/rum-dashboard/checkpoint-by-url.sql
+++ src/queries/rum-dashboard/checkpoint-by-url.sql
@@ -1,21 +1,39 @@
---- description: Get URL Request for Quotes Data From RUM for a given domain
+--- description: Get URL Specific Daily Conversion Data From RUM for a given domain
 --- Authorization: none
 --- Access-Control-Allow-Origin: *
 --- limit: 30
 --- interval: 30
 --- offset: 0
+--- startdate: 2023-02-01
+--- enddate: 2023-05-28
 --- timezone: UTC
 --- exactmatch: true
 --- url: -
 --- device: all
 --- domainkey: secret
-with rfqs AS (
+--- ckpt: search
+with sidekick_events AS (
 SELECT
-*
+  FORMAT_DATE("%Y-%m-%d", DATE_TRUNC(time, DAY)) AS day,
+  id,
+  checkpoint,
+  hostname,
+  url,
+  pageviews,
+  source,
+  target
   FROM   helix_rum.CHECKPOINTS_V4( @url, @offset, @interval, '-', '-', 'UTC', 'all', @domainkey )
 WHERE 
-  checkpoint LIKE "%rfq%" AND 
-  (
+  checkpoint LIKE @ckpt
+
+)
+SELECT   url,
+         checkpoint,
+         target,
+         sum(pageviews) AS invocations,
+FROM     sidekick_events
+WHERE
+(
        (
        true = true
        AND (
@@ -27,9 +45,7 @@ WHERE
               or
               url = concat('https://', REGEXP_REPLACE(@url, 'https://www.', ''))
               )
-       ) OR       @exactmatch = false )
-), 
-unique_targets as (
-  select (case when not @exactmatch then hostname end) as hostname,(case when @exactmatch then url end) as url, lower(target) as target, sum(pageviews) traffic from rfqs group by (case when not @exactmatch then hostname end), lower(target), (case when @exactmatch then url end)
-)
-select hostname, url, target, sum(traffic) as traffic from unique_targets where target is not null group by hostname, url, target order by traffic desc
+       ) OR       true = false )
+GROUP BY checkpoint,
+         url, target
+ORDER BY invocations desc;
