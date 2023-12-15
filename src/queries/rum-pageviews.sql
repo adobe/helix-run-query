c53e6c1ac7bad95c2ce259d9d074e4593081d083
Marquise Rosier <mrosier@Marquises-MacBook-Pro.local>
2023-12-15 01:53:47 -0800

    remove last part

--- src/queries/rum-pageviews.sql
+++ src/queries/rum-pageviews.sql
@@ -108,28 +108,3 @@ BEGIN
   )
 
   SELECT * FROM finaldata ORDER BY time DESC;
-  SET results = (SELECT SUM(pageviews) FROM (SELECT * FROM temp_pageviews));
-END;
-IF (CAST(@granularity AS STRING) = "auto") THEN
-    CALL helix_rum.MODIFIED_PAGEVIEWS_V3(1, CAST(@interval AS INT64), CAST(@offset AS INT64), @url, @timezone, @domainkey, results);
-    IF (results > (CAST(@interval AS INT64) * 200)) THEN
-        # we have enough results, use the daily granularity
-        SELECT * FROM temp_pageviews;
-    ELSE
-        # we don't have enough results, zoom out
-        DROP TABLE temp_pageviews;
-        CALL helix_rum.MODIFIED_PAGEVIEWS_V3(7, CAST(@interval AS INT64), CAST(@offset AS INT64), @url, @timezone, @domainkey, results);
-        IF (results > (CAST(@interval AS INT64) * 200)) THEN
-            # we have enough results, use the weekly granularity
-            SELECT * FROM temp_pageviews;
-        ELSE
-            # we don't have enough results, zoom out to monthly and stop
-            DROP TABLE temp_pageviews;
-            CALL helix_rum.MODIFIED_PAGEVIEWS_V3(30, CAST(@interval AS INT64), CAST(@offset AS INT64), @url, @timezone, @domainkey, results);
-            SELECT * FROM temp_pageviews;
-        END IF;
-    END IF;
-ELSE
-    CALL helix_rum.MODIFIED_PAGEVIEWS_V3(CAST(@granularity AS INT64), CAST(@interval AS INT64), CAST(@offset AS INT64), @url, @timezone, @domainkey, results);
-    SELECT * FROM temp_pageviews;
-END IF;
