86b1a5337cd6e5c33834b95e4c33698401a3cb9a
Lars Trieloff <lars@trieloff.net>
2024-03-07 20:03:43 +0100

    feat(bundles): experimental raw data export for single day of data

---
+++ src/queries/rum-bundles.sql
@@ -0,0 +1,32 @@
+--- description: get raw rum data for a given day and base URL
+--- Cache-Control: max-age=86400
+--- url: -
+--- startdate: 2024-01-01
+--- enddate: 2024-01-02
+--- domainkey: secret
+
+SELECT
+  id,
+  time,
+  url,
+  checkpoint,
+  source,
+  target,
+  LCP,
+  INP,
+  CLS,
+  TTFB
+FROM helix_rum.EVENTS_V5(
+  @url,
+  -1,
+  -1,
+  @startdate,
+  # startdate plus one day
+  FORMAT_TIMESTAMP(
+    "%Y-%m-%d",
+    TIMESTAMP_ADD(TIMESTAMP(@startdate), INTERVAL 1 DAY)
+  ),
+  "UTC",
+  "all",
+  @domainkey
+)
