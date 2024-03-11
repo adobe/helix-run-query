3915bf8a4d0ab3341c2e77517bd314af2a2c869f
Lars Trieloff <lars@trieloff.net>
2024-03-11 22:26:32 +0100

    test(multi-results): add tests for struct and array responses

--- src/queries/multi-results.sql
+++ src/queries/multi-results.sql
@@ -1,14 +1,29 @@
 CREATE OR REPLACE TEMP TABLE all_results
 (
-  is_admin BOOLEAN
+  is_admin BOOLEAN,
+  events ARRAY<STRING>,
+  metadata STRUCT<foo STRING, bar INT>,
+  meta_array ARRAY<STRUCT<foo STRING, bar INT>>
 );
 
 INSERT INTO all_results
-(is_admin)
+(is_admin, events, metadata, meta_array)
 VALUES
-(false);
+(
+  false,
+  ARRAY['{"event": "page_view", "url": "/"}',
+        '{"event": "page_view", "url": "/about"}',
+        '{"event": "page_view", "url": "/contact"}'],
+  STRUCT('foo', 2),
+  [STRUCT('foo', 2), STRUCT('foo', 3)]
+);
 
 # hlx:metadata
 SELECT 100 AS total_rows;
 
-SELECT is_admin FROM all_results LIMIT 1;
+SELECT
+  is_admin,
+  events,
+  metadata,
+  meta_array
+FROM all_results LIMIT 1;
