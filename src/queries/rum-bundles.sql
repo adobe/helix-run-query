9f71873a2b5aafa591d03f04f8f97ffd4e2710d1
Lars Trieloff <lars@trieloff.net>
2024-03-11 20:39:38 +0100

    feat(bundles): actually create a bundle per ID

--- src/queries/rum-bundles.sql
+++ src/queries/rum-bundles.sql
@@ -7,28 +7,45 @@
 
 SELECT
   id,
-  time,
-  url,
-  checkpoint,
-  source,
-  target,
-  weight,
-  user_agent,
-  LCP,
-  INP,
-  CLS,
-  TTFB
-FROM helix_rum.EVENTS_V5(
-  @url,
-  -1,
-  -1,
-  @startdate,
-  # startdate plus one day
-  FORMAT_TIMESTAMP(
-    "%Y-%m-%d",
-    TIMESTAMP_ADD(TIMESTAMP(@startdate), INTERVAL 1 DAY)
-  ),
-  "UTC",
-  "all",
-  @domainkey
-)
+  STRING(MIN(time)) AS time, -- noqa: RF04
+  CAST(ANY_VALUE(weight) AS INT64) AS weight,
+  ARRAY_AGG(
+    STRUCT(
+      STRING(time) AS time, -- noqa: RF04
+      url,
+      CASE
+        WHEN LCP IS NOT NULL THEN 'cwv-lcp'
+        WHEN INP IS NOT NULL THEN 'cwv-inp'
+        WHEN CLS IS NOT NULL THEN 'cwv-cls'
+        WHEN TTFB IS NOT NULL THEN 'cwv-ttfb'
+        ELSE checkpoint
+      END AS checkpoint,
+      source,
+      target,
+      CASE
+        WHEN LCP IS NOT NULL THEN LCP
+        WHEN INP IS NOT NULL THEN INP
+        WHEN CLS IS NOT NULL THEN CLS
+        WHEN TTFB IS NOT NULL THEN TTFB
+      END AS value, -- noqa: RF04
+      user_agent
+    )
+  ) AS events
+FROM
+  helix_rum.EVENTS_V5(
+    @url,
+    -1,
+    -1,
+    @startdate,
+    # startdate plus one day
+    FORMAT_TIMESTAMP(
+      "%Y-%m-%d",
+      TIMESTAMP_ADD(TIMESTAMP(@startdate), INTERVAL 1 DAY)
+    ),
+    "UTC",
+    "all",
+    @domainkey
+  )
+GROUP BY
+  id,
+  TIMESTAMP_TRUNC(time, DAY)
