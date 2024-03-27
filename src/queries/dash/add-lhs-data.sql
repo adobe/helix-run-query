3aca3dd682dc6005e891c596b276e3461eb84711
Marquise Rosier <mrosier@Marquises-MacBook-Pro.local>
2024-03-27 06:35:18 -0700

    feat(add-lhs): lint-fixes

--- src/queries/dash/add-lhs-data.sql
+++ src/queries/dash/add-lhs-data.sql
@@ -1,24 +1,24 @@
-  --- description: Add lhs data to bigquery
-  --- Access-Control-Allow-Origin: *
-  --- timezone: UTC
-  --- url: -
-  --- domainkey: secret
-  --- perf_score: -1
-  --- acc_score: -1
-  --- bp_score: -1
-  --- seo_score: -1
-  --- perf_tti_score: -1
-  --- perf_speed_idx: -1
-  --- seo_crawl_score: -1
-  --- seo_crawl_anchors_score: -1
-  --- net_servr_time: -1
-  --- net_nl: -1
-  --- net_mainthread_work_score: -1
-  --- net_total_blocking_score: -1
-  --- net_img_optimization_score: -1
-  --- third_party_score: -1
-  --- device_type: -
-  --- time: -
+--- description: Add lhs data to bigquery
+--- Access-Control-Allow-Origin: *
+--- timezone: UTC
+--- url: -
+--- domainkey: secret
+--- perf_score: -1
+--- acc_score: -1
+--- bp_score: -1
+--- seo_score: -1
+--- perf_tti_score: -1
+--- perf_speed_idx: -1
+--- seo_crawl_score: -1
+--- seo_crawl_anchors_score: -1
+--- net_servr_time: -1
+--- net_nl: -1
+--- net_mainthread_work_score: -1
+--- net_total_blocking_score: -1
+--- net_img_optimization_score: -1
+--- third_party_score: -1
+--- device_type: -
+--- time: -
 IF EXISTS (
   SELECT
     hostname_prefix,
@@ -28,13 +28,18 @@ IF EXISTS (
     `helix-225321.helix_reporting.domain_keys`
   WHERE
     key_bytes = SHA512(@domainkey)
-    AND (revoke_date IS NULL
+    AND (
+      revoke_date IS NULL
       OR revoke_date > CURRENT_DATE(@timezone))
-    AND (hostname_prefix = ""
-      OR hostname_prefix = @url)
-    AND readonly = FALSE ) THEN
-CALL
-  helix_external_data.ADD_LHS_DATA( @url,
+    AND (
+      hostname_prefix = ""
+      OR hostname_prefix = @url
+    )
+    AND readonly = FALSE
+) THEN
+  CALL
+  helix_external_data.ADD_LHS_DATA (
+    @url,
     @perf_score,
     @acc_score,
     @bp_score,
@@ -51,9 +56,8 @@ CALL
     @third_party_score,
     @device_type,
     @time,
-    @audit_ref );
-END IF
-  ;
+    @audit_ref);
+END IF;
 SELECT
   "Lighthouse Data Successfully Uploaded" AS status,
   url,
@@ -68,3 +72,4 @@ WHERE
 GROUP BY
   status,
   url
+  
