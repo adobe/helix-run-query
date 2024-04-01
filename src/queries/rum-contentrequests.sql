ea1ffeb5c37f8f15ed82fbfbddead8061bca0373
puric <puric@adobe.com>
2024-04-01 18:44:00 +0200

    refactor: add lower and upper bound calculations,  add pagination

--- src/queries/rum-contentrequests.sql
+++ src/queries/rum-contentrequests.sql
@@ -7,8 +7,9 @@
 --- enddate: 2023-12-31
 --- url: -
 --- granularity: 1
---- aggregate: - (default) or 'reduce'
 --- timezone: UTC
+--- after: -
+--- limit: 1000
 --- domainkey: secret
 WITH all_raw_events AS (
   SELECT
@@ -34,303 +35,242 @@ WITH all_raw_events AS (
   )
 ),
 
-
 -- IDs can repeat, so we group by hostname and day
 group_all_events_daily AS (
   SELECT
     id,
     hostname,
     trunc_time,
-    ARRAY_AGG(DISTINCT checkpoint IGNORE NULLS) AS checkpoint
-  FROM all_raw_events
-  GROUP BY id, hostname, trunc_time
-),
-
--- filter PageViews: HTML requests
-pageviews AS (
-  SELECT DISTINCT
-    all_raw_events.id,
-    all_raw_events.weight,
-    all_raw_events.url,
-    all_raw_events.hostname,
-    all_raw_events.trunc_time,
-    all_raw_events.weight AS requests
-  FROM group_all_events_daily
-  LEFT JOIN all_raw_events
-    ON
-      group_all_events_daily.id = all_raw_events.id
-      AND group_all_events_daily.hostname = all_raw_events.hostname
-      AND group_all_events_daily.trunc_time = all_raw_events.trunc_time
-  -- PageView = any ID that does not have a checkpoint=404
-  WHERE '404' NOT IN UNNEST(group_all_events_daily.checkpoint)
-  -- bots are excluded
-  AND all_raw_events.user_agent != 'bot'
-),
-
--- filter API Calls: JSON requests
-apicalls AS (
-  SELECT
-    id,
-    hostname,
-    url,
     weight,
-    user_agent,
-    trunc_time,
-    SUM(weight) AS requests
+    # an ID marks a single HTML request
+    weight AS html_requests,
+    # a JSON request
+    COALESCE(
+      COUNTIF(checkpoint = 'loadresource' AND target NOT LIKE '%.json'), 0
+    )
+    * weight AS json_requests,
+    # request by bot
+    COALESCE(user_agent = 'bot', false) AS is_bot_request,
+    # request with 404 error
+    COALESCE((COUNTIF(checkpoint = '404') > 0), false) AS is_404_request
   FROM all_raw_events
-  -- APICall = any loadresource event that has a target that does not end in .json
-  WHERE (checkpoint = 'loadresource' AND target NOT LIKE '%.json')
-  -- bots are excluded
-  AND user_agent != 'bot'
-  GROUP BY id, url, hostname, weight, user_agent, trunc_time
-),
-
--- filter 404 requests
-error404 AS (
-  SELECT DISTINCT
-    all_raw_events.id,
-    all_raw_events.weight,
-    all_raw_events.url,
-    all_raw_events.hostname,
-    all_raw_events.trunc_time,
-    all_raw_events.weight AS requests
-  FROM group_all_events_daily
-  LEFT JOIN all_raw_events
-    ON
-      group_all_events_daily.id = all_raw_events.id
-      AND group_all_events_daily.hostname = all_raw_events.hostname
-      AND group_all_events_daily.trunc_time = all_raw_events.trunc_time
-  WHERE '404' IN UNNEST(group_all_events_daily.checkpoint)
-),
-
--- filter bot requests
-bots AS (
-  SELECT
-    id,
-    hostname,
-    url,
-    weight,
-    user_agent,
-    trunc_time,
-    SUM(weight) AS requests
-  FROM all_raw_events
-  WHERE user_agent = 'bot'
-  GROUP BY id, url, hostname, weight, user_agent, trunc_time
-),
-
-group_all_events_with_details_daily AS (
-  SELECT
-    id,
-    hostname,
-    url,
-    weight,
-    user_agent,
-    trunc_time
-  FROM all_raw_events
-  GROUP BY id, hostname, url, weight, user_agent, trunc_time
+  GROUP BY id, hostname, trunc_time, weight, user_agent
 ),
 
 -- filter requests
 dailydata AS (
   SELECT
-    e.hostname,
-    e.url,
-    e.weight,
-    e.user_agent,
-    e.trunc_time,
-    -- HTML requests: PageViews
-    SUM(COALESCE(pv.requests, 0)) AS html_requests,
-    -- JSON requests: APICalls
-    SUM(COALESCE(ac.requests, 0)) AS json_requests,
-    -- bot requests
-    SUM(COALESCE(b.requests, 0)) AS bot_requests,
+    hostname,
+    trunc_time,
+    weight,
+    # 1 PageView = 1 ContentRequest
+    # 5 APICalls = 1 PageView = 1 ContentRequest
+    # Exclude Bots and 404
+    SUM(
+      CASE
+        WHEN is_bot_request OR is_404_request THEN 0
+        ELSE (html_requests + (json_requests * 0.2))
+      END
+    ) AS content_requests,
+    -- PageViews
+    SUM(
+      CASE
+        WHEN is_bot_request OR is_404_request THEN 0
+        ELSE html_requests
+      END
+    ) AS pageviews,
+    -- APICalls
+    SUM(
+      CASE
+        WHEN is_bot_request OR is_404_request THEN 0
+        ELSE json_requests
+      END
+    ) AS apicalls,
+    -- HTML requests: Total HTML requests
+    SUM(html_requests) AS html_requests,
+    -- JSON requests: Total JSON requests
+    SUM(json_requests) AS json_requests,
     -- 404 requests
-    SUM(COALESCE(er.requests, 0)) AS error404_requests,
-    -- 1 PageView = 1 ContentRequest
-    -- 5 APICalls = 1 PageView = 1 ContentRequest
-    SUM(COALESCE(pv.requests, 0))
-    + SUM(COALESCE(ac.requests, 0) * 0.2) AS content_requests
-  FROM group_all_events_with_details_daily AS e
-  LEFT JOIN pageviews AS pv
-    ON
-      e.id = pv.id
-      AND e.hostname = pv.hostname
-      AND e.url = pv.url
-      AND e.weight = pv.weight
-      AND e.trunc_time = pv.trunc_time
-  LEFT JOIN apicalls AS ac
-    ON
-      e.id = ac.id
-      AND e.hostname = ac.hostname
-      AND e.url = ac.url
-      AND e.weight = ac.weight
-      AND e.trunc_time = ac.trunc_time
-  LEFT JOIN error404 AS er
-    ON
-      e.id = er.id
-      AND e.hostname = er.hostname
-      AND e.url = er.url
-      AND e.weight = er.weight
-      AND e.trunc_time = er.trunc_time
-  LEFT JOIN bots AS b
-    ON
-      e.id = b.id
-      AND e.hostname = b.hostname
-      AND e.url = b.url
-      AND e.weight = b.weight
-      AND e.trunc_time = b.trunc_time
-  GROUP BY e.hostname, e.url, e.weight, e.user_agent, e.trunc_time
+    CASE
+      WHEN is_bot_request THEN SUM(html_requests)
+      ELSE 0
+    END AS error404_requests,
+    -- Bot requests
+    CASE
+      WHEN is_bot_request THEN SUM(html_requests)
+      ELSE 0
+    END AS bot_html_requests,
+    CASE
+      WHEN is_bot_request THEN SUM(json_requests)
+      ELSE 0
+    END AS bot_json_requests
+  FROM group_all_events_daily
+  GROUP BY hostname, trunc_time, weight, is_bot_request, is_404_request
 ),
 
 monthlydata AS (
   SELECT
     hostname,
-    url,
-    weight,
-    user_agent,
     TIMESTAMP_TRUNC(trunc_time, MONTH) AS trunc_time,
+    weight,
+    SUM(content_requests) AS content_requests,
+    SUM(pageviews) AS pageviews,
+    SUM(apicalls) AS apicalls,
     SUM(html_requests) AS html_requests,
     SUM(json_requests) AS json_requests,
-    SUM(bot_requests) AS bot_requests,
     SUM(error404_requests) AS error404_requests,
-    SUM(content_requests) AS content_requests
+    SUM(bot_html_requests) AS bot_html_requests,
+    SUM(bot_json_requests) AS bot_json_requests
   FROM dailydata
   GROUP BY
-    hostname, url, weight, user_agent,
+    hostname, weight,
     TIMESTAMP_TRUNC(trunc_time, MONTH)
 ),
 
 yearlydata AS (
   SELECT
     hostname,
-    url,
-    weight,
-    user_agent,
     TIMESTAMP_TRUNC(trunc_time, YEAR) AS trunc_time,
+    weight,
+    SUM(content_requests) AS content_requests,
+    SUM(pageviews) AS pageviews,
+    SUM(apicalls) AS apicalls,
     SUM(html_requests) AS html_requests,
     SUM(json_requests) AS json_requests,
-    SUM(bot_requests) AS bot_requests,
     SUM(error404_requests) AS error404_requests,
-    SUM(content_requests) AS content_requests
+    SUM(bot_html_requests) AS bot_html_requests,
+    SUM(bot_json_requests) AS bot_json_requests
   FROM dailydata
   GROUP BY
-    hostname, url, weight, user_agent,
+    hostname, weight,
     TIMESTAMP_TRUNC(trunc_time, YEAR)
 ),
 
-all_data AS (
-  # Desired granularity
-  SELECT * FROM dailydata WHERE CAST(@granularity AS INT64) = 1
+alldata_granularity AS (
+  --- Desired granularity
+  SELECT
+    CONCAT(hostname, '-', CAST(weight AS STRING), '-', CAST(UNIX_MICROS(trunc_time) AS STRING)) AS row_id,
+    *
+  FROM dailydata WHERE CAST(@granularity AS INT64) = 1
   UNION ALL
-  SELECT * FROM monthlydata WHERE CAST(@granularity AS INT64) = 30
+  SELECT
+    CONCAT(hostname, '-', CAST(weight AS STRING), '-', CAST(UNIX_MICROS(trunc_time) AS STRING)) AS row_id,
+    *
+  FROM monthlydata WHERE CAST(@granularity AS INT64) = 30
   UNION ALL
-  SELECT * FROM yearlydata WHERE CAST(@granularity AS INT64) = 365
+  SELECT
+    CONCAT(hostname, '-', CAST(weight AS STRING), '-', CAST(UNIX_MICROS(trunc_time) AS STRING)) AS row_id,
+    *
+  FROM yearlydata WHERE CAST(@granularity AS INT64) = 365
 ),
 
-aggregate_default AS (
+alldata AS (
   SELECT
+    row_id,
     trunc_time,
     hostname,
-    url,
     weight,
-    user_agent,
-    html_requests,
-    json_requests,
-    bot_requests,
-    error404_requests,
-    content_requests,
     EXTRACT(YEAR FROM TIMESTAMP_TRUNC(trunc_time, YEAR)) AS year,
     EXTRACT(MONTH FROM TIMESTAMP_TRUNC(trunc_time, MONTH)) AS month,
-    EXTRACT(DAY FROM TIMESTAMP_TRUNC(trunc_time, DAY)) AS day
-  FROM all_data
-  ORDER BY year DESC, month DESC, day DESC
-),
-
--- reduce to 1 value per hostname, per date, per content type
-aggregate_reduce AS (
-  SELECT
-    year,
-    month,
-    day,
-    trunc_time,
-    hostname,
-    'all' AS url,
-    ARRAY_TO_STRING(
-      ARRAY_AGG(DISTINCT user_agent IGNORE NULLS),
-      ', '
-    ) AS user_agent,
-    AVG(weight) AS weight,
+    EXTRACT(DAY FROM TIMESTAMP_TRUNC(trunc_time, DAY)) AS day,
+    SUM(pageviews) AS pageviews,
+    SUM(apicalls) AS apicalls,
+    SUM(content_requests) AS content_requests,
     SUM(html_requests) AS html_requests,
     SUM(json_requests) AS json_requests,
-    SUM(bot_requests) AS bot_requests,
     SUM(error404_requests) AS error404_requests,
-    SUM(content_requests) AS content_requests
-  FROM aggregate_default
-  GROUP BY year, month, day, trunc_time, hostname
-  ORDER BY year DESC, month DESC, day DESC
+    SUM(bot_html_requests) AS bot_html_requests,
+    SUM(bot_json_requests) AS bot_json_requests,
+    CASE
+      # no sampling, confidence level = 100%
+      WHEN weight = 1 THEN 100
+      # otherwise 95% confidence level, industry standard
+      ELSE 95
+    END AS confidence_level,
+    # get margin of error for sampling
+    helix_rum.CALC_BINOMIAL_DISTRIBUTION_MARGIN_OF_ERROR_TEMP(
+      # sampling rate
+      weight,
+      # successes 
+      (SUM(content_requests) * (1 / weight)),
+      # z-score
+      CAST(CASE
+        # no sampling, confidence level = 100%
+        WHEN weight = 1 THEN 0
+        # otherwise 95% confidence level, industry standard
+        ELSE 1.96
+      END AS NUMERIC)
+    ) AS margin_of_error,
+    # row number
+    ROW_NUMBER()
+      OVER (ORDER BY hostname ASC, trunc_time ASC, weight ASC)
+      AS rownum,
+    row_id = CAST(@after AS STRING) AS is_cursor
+  FROM alldata_granularity
+  GROUP BY row_id, year, month, day, trunc_time, hostname, weight
+  ORDER BY hostname ASC, trunc_time ASC, weight ASC
 ),
 
-aggregate_result AS (
-  SELECT
-    year,
-    month,
-    day,
-    trunc_time,
-    hostname,
-    url,
-    weight,
-    user_agent,
-    html_requests,
-    json_requests,
-    bot_requests,
-    error404_requests,
-    content_requests
-  FROM aggregate_reduce
-  WHERE @aggregate = 'reduce'
+cursor_rows AS (
+  SELECT MIN(rownum) AS rownum FROM alldata WHERE is_cursor
   UNION ALL
-  SELECT
-    year,
-    month,
-    day,
-    trunc_time,
-    hostname,
-    url,
-    weight,
-    user_agent,
-    html_requests,
-    json_requests,
-    bot_requests,
-    error404_requests,
-    content_requests
-  FROM aggregate_default
-  WHERE @aggregate != 'reduce'
+  SELECT 0 AS rownum FROM alldata WHERE @after = '-'
+),
+
+cursor_rownum AS (
+  SELECT MIN(rownum) AS rownum FROM cursor_rows
 )
 
 SELECT
+  row_id AS id,
   year,
   month,
   day,
-  hostname,
-  url,
+  hostname, -- noqa: RF04
   weight,
-  user_agent,
+  content_requests,
+  confidence_level,
+  margin_of_error,
+  pageviews,
+  # Lower Bound
+  # Formula: Content Requests - Margin of Error
+  # Make sure lower bound cannot be < 1
+  apicalls,
+  # Upper Bound
+  # Formula: Content Requests + Margin of Error
   html_requests,
   json_requests,
-  bot_requests,
   error404_requests,
-  content_requests,
-  CAST(trunc_time AS STRING) AS trunc_time
-FROM aggregate_result
+  bot_html_requests,
+  bot_json_requests,
+  rownum,
+  FORMAT_TIMESTAMP('%Y-%m-%dT%X%Ez', trunc_time) AS time, -- noqa: RF04
+  CAST(GREATEST((content_requests - margin_of_error), 1) AS INT64)
+    AS lower_bound,
+  CAST((content_requests + margin_of_error) AS INT64) AS upper_bound
+FROM alldata
+WHERE
+  (rownum > (SELECT rownum FROM cursor_rownum))
+  AND (rownum <= ((SELECT rownum FROM cursor_rownum) + @limit))
+ORDER BY
+  rownum ASC
+-- id: the cursor id
 -- year: the year of the beginning of the reporting interval
 -- month: the month of the beginning of the reporting interval
 -- day: the day of the beginning of the reporting interval
+-- time: the timestamp of the beginning of the reporting interval
 -- hostname: the domain itself
--- url: the URL of the request ('all' in 'reduce' result)
--- weight: the sampling weight (average weight in 'reduce' result)
--- user_agent: the user agent of the requests
--- html_requests: the number of PageViews
--- json_requests: the number of APICalls
--- bot_requests: the number of requests by bots
--- error404_requests: the number of requests returning 404 error
+-- weight: the sampling weight
 -- content_requests: the number of Content Requests in the reporting interval that match the criteria
--- trunc_time: the timestamp of the beginning of the reporting interval
+-- confidence_level: the level of confidence for the Content Requests calculation
+-- margin_of_error: the margin of error  for the Content Requests calculation
+-- lower_bound: the lower bound of Content Requests respecting the sampling error
+-- upper_bound: the upper bound of Content Requests respecting the sampling error
+-- pageviews: the number of PageViews
+-- apicalls: the number of APICalls
+-- html_requests: the total number of HTML Requests
+-- json_requests: the total number of JSON Requests
+-- error404_requests: the total number of requests returning 404 error
+-- bot_html_requests: the total number of HTML requests by bots
+-- bot_json_requests: the total number of JSON requests by bots
+-- rownum: the number of the row
